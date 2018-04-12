75a0334bce6fb0fd51a67059fcfd73d40c5332e1
Knean <mario009c@yahoo.com>
2018-04-12 15:15:47 +0100

    commiting before push

--- booksite/book/urls.py
+++ booksite/book/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.conf.urls import url
 from django.urls import path
+app_name='book'
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:chapter_id>/',views.detailchapter,name = 'detail_chapter'),
--- booksite/book/views.py
+++ booksite/book/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Chapter
+from django.template import Template
+from django.template.loader import get_template
+from django.http import Http404
 # Create your views here.
 def home(request):
-	return HttpResponse("this is just the beginning")
+	template=get_template('book/index.html')
+	chapter_list=Chapter.objects.all()
+	
+	return HttpResponse(template.render({'chapter_list':chapter_list}))
+	
+
 def detailchapter(request, chapter_id):
-    	chapter_story= Chapter.objects.get(id=chapter_id).chapter_text
-    	return HttpResponse('its a chapter numbered: '+str(chapter_id)+'and it goes like this:<br> '+str(chapter_story))
+	try:
+		chapter_story= Chapter.objects.get(id=chapter_id).chapter_text
+	except Chapter.DoesNotExist:
+		raise Http404('no such book')
+	return HttpResponse('its a chapter numbered: '+str(chapter_id)+'and it goes like this:<br> '+str(chapter_story))
