d9973a5b8e5abe0f410e38dcde34f3643a86bef1
Knean <mario009c@yahoo.com>
2018-04-24 09:22:46 +0100

    finished the testing stuff

--- booksite/book/tests.py
+++ booksite/book/tests.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
 from .models import Answer, Chapter
 from django.utils import timezone
+from django.urls import reverse
 # Create your tests here.
 class some_silly_test(TestCase):
     def test_this_is_a_retarded_test(self):
-        print(Chapter.objects.all())
+        
         mario=Chapter.objects.create(chapter_text="lalala",create_date=timezone.now())
-        print(mario.chapter_text)
-        self.assertIs(mario.id==2,True)
+       
+        self.assertIs(mario.id==1,True)
+    def testing_if_client_works(self):
+        response = self.client.get(reverse('book:home'))
+        print('response: ',response.content, '\n','context',response.context)
+        self.assertIs(response==True,True)
