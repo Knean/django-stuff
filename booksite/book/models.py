2b31d19ffd03c2b1f0d58268f3417a1299073737
Knean <mario009c@yahoo.com>
2018-05-05 14:21:28 +0100

    starting work on validators

--- booksite/book/models.py
+++ booksite/book/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Chapter(models.Model):
@@ -7,11 +8,24 @@ class Chapter(models.Model):
 	create_date = models.DateTimeField('date created')
 	def __str__(self):
 		return self.chapter_text
+		#get active user. profile
+		#calcualte where to which chapter each answer leads to
+		# based on players previous choices
+
 
 class Answer(models.Model):
-	chapter=models.ForeignKey(Chapter,on_delete=models.CASCADE)
+	chapter=models.OneToOneField(Chapter,on_delete=models.CASCADE)
 	answer_text=models.TextField()
 	answer_image = models.FileField(upload_to='uploads/')
 	def __str__(self):
 		return self.answer_text
 	
+    		
+
+class UserProfile(models.Model):
+	player=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+	answers=models.ManyToManyField(Answer)
+	
+def validate_only_one_answer(value):
+	for answer in players.answers.all:
+		if answer.chapter
