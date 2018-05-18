dff9c4d50d8bba183ec6957611905ad919c2792a
Knean <mario009c@yahoo.com>
2018-05-18 20:19:24 +0100

    still being raped with form field update

--- booksite/book/forms.py
+++ booksite/book/forms.py
@@ -1,5 +1,6 @@
-from .models import Profile
+from .models import Profile, Chapter
 from django.forms import ModelForm
+from django import forms
 from django.core.exceptions import ValidationError
 
 class ProfileForm(ModelForm):
@@ -17,3 +18,19 @@ class ProfileForm(ModelForm):
                 if answers.all()[n].chapter==answers.all()[b].chapter and b !=n:
 
                     raise ValidationError('only one answer per chapter')
+
+class ChapterForm(ModelForm):
+    field1 = forms.ModelChoiceField(queryset=None, empty_label="(Nothing)")
+    '''def __init__(self, *args, **kwargs):
+        self.fields['field1'].queryset=kwargs.pop('user',None)
+        #forms.ModelChoiceField(queryset=kwargs.pop('user',None), empty_label="(Nothing)")
+        print('aaaaaaaaaaargs',kwargs)
+        super().__init__(*args, **kwargs)'''
+    #self.fields['field1'].queryset = self.request.user.profile
+    class Meta:
+        model=Chapter
+        fields = '__all__'
+    
+    #mario1 = forms.CharField()
+
+
